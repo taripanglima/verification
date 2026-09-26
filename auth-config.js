4eaba314606151bfba5fd93b2920ccc54236eaff
taripanglima <taripanglima@gmail.com>
2026-09-27 00:51:12 +0800

    Delete auth-config.js

--- auth-config.js
+++
@@ -1,6 +0,0 @@
-// Replace these two values with your Supabase project values.
-window.PGPM_AUTH = {
-  SUPABASE_URL: "https://pqrlqfooufhgblomnrqd.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_AdKhI1wugEGQBkGCG_zHTg_F8dBRxO0"
-};
-window.PGPM_AUTH_REDIRECT = "https://verificationpgpm.pages.dev";
